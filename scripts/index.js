09186a4b2cc4fdb44c2a180b196e9629b55072f5
Anastasia Esko <nastyho00@mail.ru>
2024-03-01 17:19:20 +0300

    on the review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+addCardsToHtml(generateCardNodes())
+function generateCardNodes() {
+    return initialCards.map(function (cardData) {
+        return generateCardNode(cardData);
+    })
+}
+function generateCardNode(cardData) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__image').setAttribute('src', cardData.link);
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (event){
+        event.currentTarget.closest('.card').remove();
+    })
+    return cardElement;
+}
+function addCardsToHtml(nodes) {
+    const placesList = document.querySelector('.places__list');
+    placesList.append(...nodes);
+}
