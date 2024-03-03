223691ef4810f48f8109016e397afbb84a1e17ef
Anastasia Esko <nastyho00@mail.ru>
2024-03-03 12:38:14 +0300

    on the review

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,24 @@
-addCardsToHtml(generateCardNodes())
-function generateCardNodes() {
-    return initialCards.map(function (cardData) {
-        return generateCardNode(cardData);
-    })
-}
+let cardNodes = initialCards.map(function (cardData) {
+    return generateCardNode(cardData);
+})
+
+addCardsToHtml(cardNodes)
 function generateCardNode(cardData) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__image').setAttribute('src', cardData.link);
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
     cardElement.querySelector('.card__title').textContent = cardData.name;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', function (event){
-        event.currentTarget.closest('.card').remove();
-    })
+    cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard)
+
     return cardElement;
 }
 function addCardsToHtml(nodes) {
     const placesList = document.querySelector('.places__list');
     placesList.append(...nodes);
+}
+
+function removeCard(event) {
+    event.currentTarget.closest('.card').remove();
 }
